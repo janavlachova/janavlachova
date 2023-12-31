63443e717375c456d1efc891196a9c6a60d42991
Jana Vlachová <65499282+janavlachova@users.noreply.github.com>
2023-12-31 09:00:41 +0100

    added logo (#10)

--- janavlachova/pages/[...slug].vue
+++ janavlachova/pages/[...slug].vue
@@ -1,6 +1,7 @@
 <template>
     <main>
-        Hello World
+        <img alt="jana vlachova logo" class="logo" src="@/static/logo.svg" width="125" height="125" />
+        jana vlachova
         <ContentDoc />
     </main>
 </template>
